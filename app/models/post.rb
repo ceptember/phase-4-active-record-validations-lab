2da950f5352991ca4d289fc4196d526c54f2e211
Christy Perozzi <christy.perozzi@gmail.com>
2022-04-06 10:48:05 -0400

    partly passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    #validate :title_is_clickbait 
+
+    def title_is_clickbait
+
+        phrases = ["Won't Believe", "Secret", "Top #{/[0-9]/}","Guess"]
+        phrases_in_title = phrases.filter { |phrase| title.include?(phrase) } 
+        
+        unless phrases_in_title.length > 0
+            errors.add(:title, "Not clickbait-y enough")
+        end
+        
+    end
+
 end
+
