cfa1d8fcb75cd9ced4cd32a7e05402966a6f2b01
Christy Perozzi <christy.perozzi@gmail.com>
2022-04-06 11:34:48 -0400

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,18 +5,15 @@ class Post < ApplicationRecord
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 
-    #validate :title_is_clickbait 
+    validate :title_is_clickbait 
 
     def title_is_clickbait
-
-        phrases = ["Won't Believe", "Secret", "Top #{/[0-9]/}","Guess"]
-        phrases_in_title = phrases.filter { |phrase| title.include?(phrase) } 
+       phrases = ["Won't Believe", "Secret", "Top #{/[0-9]/}","Guess"]
+        phrases_in_title = phrases.filter { |phrase| title&.include?(phrase) } 
         
         unless phrases_in_title.length > 0
             errors.add(:title, "Not clickbait-y enough")
         end
-        
     end
-
 end
 
